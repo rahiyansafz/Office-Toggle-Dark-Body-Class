857e4c984a0ee7756adb4c0042a899ce9c37a42a
rahiyansafz <rahiyansafin@gmail.com>
2020-12-03 03:44:56 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-change-body-css-class-cfcu3j
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-change-body-css-class-cfcu3j)
---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  availableClasses: string[] = ["dark", "light"];
+  currentClassIdx: number = 0;
+
+  bodyClass: string;
+
+  constructor() {
+    this.bodyClass = this.availableClasses[this.currentClassIdx];
+    this.changeBodyClass();
+  }
+
+  changeBodyClass() {
+    // get html body element
+    const bodyElement = document.body;
+
+    if (bodyElement) {
+      this.currentClassIdx = this.getNextClassIdx();
+      const nextClass = this.availableClasses[this.currentClassIdx];
+      const activeClass = this.availableClasses[this.getPrevClassIdx()];
+
+      // remove existing class (needed if theme is being changed)
+      bodyElement.classList.remove(activeClass);
+      // add next theme class
+      bodyElement.classList.add(nextClass);
+
+      this.bodyClass = nextClass;
+    }
+  }
+
+  getPrevClassIdx(): number {
+    return this.currentClassIdx === 0
+      ? this.availableClasses.length - 1
+      : this.currentClassIdx - 1;
+  }
+
+  getNextClassIdx(): number {
+    return this.currentClassIdx === this.availableClasses.length - 1
+      ? 0
+      : this.currentClassIdx + 1;
+  }
+}
