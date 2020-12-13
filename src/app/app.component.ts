d621dd988ca083165940b3a2ed65d0fc8ca81906
rahiyansafz <rahiyansafin@gmail.com>
2020-12-13 11:04:06 +0600

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, VERSION } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  availableClasses: string[] = ["dark", "light"];
+  availableClasses: string[] = ["Dark", "Light"];
   currentClassIdx: number = 0;
 
   bodyClass: string;
